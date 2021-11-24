25bc426a7afa0a20cdb1cec8b937a42331ffdfc1
gabybuchnik <gabybuchnik@gmail.com>
2021-11-24 11:26:43 +0200

    api fix

--- FileSystem/api/route/tasks.js
+++ FileSystem/api/route/tasks.js
@@ -1,40 +1,55 @@
 const express = require('express');
 const router = express.Router();
 
-let tasks = { tasks: [{ "number": "1", "name": "make lunch" }, { "number": "2", "name": "make dinner" }, { "number": "3", "name": "make breakfast" }] };
+let tasks = [{ "number": "1", "name": "make lunch" }, { "number": "2", "name": "make dinner" }, { "number": "3", "name": "make breakfast" }];
 
 router.get('/task', (req, res, next) => {
     let { taskId } = req.query;
     if (!taskId) {
         next();
     }
-    const tasksList = { ...tasks.tasks };
-    for (let index in tasksList) {
-        if (tasksList[index].number === taskId) {
-            res.status(200).json(tasksList[index]);
-        }
+    const tasksList = [...tasks];
+
+    const found = tasksList.find(item => item.number === taskId)
+    if(found){
+        res.status(200).json(found);
+    }
+    else{
+        res.status(404).send('something wrong...');
     }
 });
 
 router.get('/task', (req, res) => {
+
     let { taskName } = req.query;
-    const tasksList = { ...tasks.tasks };
-    for (let index in tasksList) {
-        if (tasksList[index].name === taskName) {
-            res.status(200).json(tasksList[index]);
-        }
+    if(!taskName){
+        next();
+    };
+    const tasksList = [...tasks];
+
+    const found = tasksList.find(item => item.name === taskName)
+    console.log(typeof found === 'undefined')
+    if(typeof found === 'undefined'){
+        console.log(1);
+        res.status(404).send('something wrong...');
     }
+    else{
+        console.log(2);
+        res.status(200).json(found);
+        
+    }
+   
 });
 
-router.post('/task', (req, res) => {
-    let { number, name } = req.body;
-    let task = { number, name }
-    tasks.tasks.push({ task });
-    res.status(200).json(tasks);
-});
+// router.post('/task', (req, res) => {
+//     let { number, name } = req.body;
+//     let task = { number, name }
+//     tasks.tasks.push({ task });
+//     res.status(200).json(tasks);
+// });
 
-router.get('/tasks', (req, res) => {
-    res.status(200).json(tasks);
-});
+// router.get('/tasks', (req, res) => {
+//     res.status(200).json(tasks);
+// });
 
 module.exports = router;
